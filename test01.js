7437f7e9c9fe7228ce850c78acacdf6c2d015c13
Administrator <jcsmnd@gmail.com>
2019-03-11 10:54:47 -0400

    basic server with parsing

--- test01.js
+++ test01.js
@@ -1,15 +1,40 @@
 //written by Myungsik Kim 03/11/2019
 var http = require('http');
-var fs = require('fs');
-var app = http.createServer(function(request, response){
-    var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
+var url = require('url');
+
+var server = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
+
+    if(_url == '/'){
+        title = 'Default';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
+    
+    console.log(queryData.id);
+    
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+        <h2>${title}</h2>
+    </head>
+
+    <body>
+        <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+    </body>
+    </html>
+    `;
+
+    response.end(template);
 });
-app.listen(3000);
+
+server.listen(3000);
