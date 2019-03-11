c83a785978bac5f75e978b6ed62831b7c94b0778
Administrator <jcsmnd@gmail.com>
2019-03-11 02:32:37 -0400

    add simple html

--- test01.js
+++ test01.js
@@ -1,2 +1,15 @@
+//written by Myungsik Kim 03/11/2019
 var http = require('http');
-var e = "123";
+var fs = require('fs');
+var app = http.createServer(function(request, response){
+    var url = request.url;
+    if(request.url == '/'){
+        url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        return response.writeHead(404);
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+});
+app.listen(3000);
